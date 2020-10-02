e079c9f6036f3424eea5b7d133db3cb6de75ffc0
Aashish Adhikari <asis.adh@gmail.com>
2020-10-02 22:08:53 +0545

    Added news API

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,3 +1,5 @@
 class NewsConstant {
-  static final API_KEY = "<Add your auth key>";
+  static final API_KEY = "";
+  static final BASE_URL =
+      "http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-02&sortBy=publishedAt&apiKey=$API_KEY";
 }
